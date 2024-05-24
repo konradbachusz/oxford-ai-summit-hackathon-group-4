034f3e9c66f430a1b7513e3a3a7215b9622232e5
Konrad Bachusz <konrad.bachusz@credera.co.uk>
2024-05-24 12:33:55 +0100

    added image input to streamlit

--- src/app.py
+++ src/app.py
@@ -3,5 +3,11 @@ import streamlit as st
 def main():
     st.title("Hello, World!")
 
+
+    picture = st.camera_input("Take a picture")
+
+    if picture:
+        st.image(picture)
+
 if __name__ == "__main__":
     main()
