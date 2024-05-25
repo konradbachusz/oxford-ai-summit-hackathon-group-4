3725b1a8e3c0b15942a67ba0c02774f64a2a4233
Ravinda Gunaratne <ravindag@hotmail.com>
2024-05-25 15:36:23 +0100

    whitespace

--- tests/features/steps/image_classification_steps.py
+++ tests/features/steps/image_classification_steps.py
@@ -6,7 +6,6 @@ from behave import given, then
 from src.notebook_funcs import classify_image
 
 
-
 @given('I have passed an image of socks to the model')
 def load_socks_image(context):
     context.image_url = 'https://media.loaf.com/image/upload/c_crop,h_857,w_1200,x_0,y_21/f_auto/q_auto/c_pad,h_786,w_1100/website/images/original/512789-sockins-sofa-sock-in-burnt-orange-stripe-full-sock-option-3.jpg'
